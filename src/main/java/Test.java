84f2e027d4260d3ee919d33102196837c93a61c1
“yangby“ <1092772495@qq.com>
2023-03-14 11:48:59 +0800

    123

---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+import util.FFmpegUtil;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/util/FFmpegUtil.java
+++ src/main/java/util/FFmpegUtil.java
@@ -8,40 +8,30 @@ import java.util.List;
 
 public class FFmpegUtil {
 
-    public final static String path = "2321";
+    public final static String path = "D:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe";
 
     /**
      * 合并视频文件
      * @param videoPathList
      */
-    public void mergeVideo(List<String> videoPathList) {
-
+    public static void mergeVideo(List<String> videoPathList) {
         if (videoPathList.size() > 1){
-
             String ffmpegPath = path;// 此处是配置地址，可自行写死如“D:\\ffmpeg\\ffmpeg.exe”
             String txtPath = "";
             try {
-                int index = videoPathList.get(0).lastIndexOf(".");
-                String newMergePath = videoPathList.get(0).substring(0, index) + "new" + videoPathList.get(0).substring(index, videoPathList.get(0).length());
-                txtPath = videoPathList.get(0).substring(0, index) + ".txt";
-                FileOutputStream fos = new FileOutputStream(new File(txtPath));
+                String firstVideo = videoPathList.get(0);
+                int index = firstVideo.lastIndexOf(".");
+                String videoName = firstVideo.substring(0, index);
+                String newMergePath = videoName + "new" + firstVideo.substring(index);
+                txtPath = videoName + ".txt";
+                FileOutputStream fos = new FileOutputStream(txtPath);
                 for (String path : videoPathList){
                     fos.write(("file '" + path + "'\r\n").getBytes());
                 }
                 fos.close();
 
                 StringBuffer command = new StringBuffer("");
-                command.append(ffmpegPath);
-                command.append(" -f");
-                command.append(" concat");
-                command.append(" -safe");
-                command.append(" 0");
-                command.append(" -i ");
-                command.append(txtPath);
-                command.append(" -c");
-                command.append(" copy ");// -c copy 避免解码，提高处理速度
-                command.append(newMergePath);
-
+                command.append(ffmpegPath).append(" -f concat -safe 0 -i ").append(txtPath).append(" -c copy ").append(newMergePath);
                 ProcessUtil.exec(command.toString(), null, 1000L);
 
                 // 删除产生的临时用来存放每个视频文件路径的txt文件
@@ -49,15 +39,15 @@ public class FFmpegUtil {
                 txtFile.delete();
 
                 // 删除原视频文件
-                for (String filePath : videoPathList){
-                    File file = new File(filePath);
-                    file.delete();
-                }
+//                for (String filePath : videoPathList){
+//                    File file = new File(filePath);
+//                    file.delete();
+//                }
 
                 // 合成的新视频文件重命名为原视频文件的第一个文件名
-                File newFile = new File(newMergePath);
-                File oldFile = new File(videoPathList.get(0));
-                newFile.renameTo(oldFile);
+//                File newFile = new File(newMergePath);
+//                File oldFile = new File(firstVideo);
+//                newFile.renameTo(oldFile);
 
             } catch (Exception e) {
                 e.printStackTrace();
